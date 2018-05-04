101981766ed64ab4d9c3999e7b24c669c051af98
Gnagnom94 <gnagnom94@gmail.com>
2018-05-04 14:25:14 +0200

    Drawer codice java. Ora funziona

--- app/src/main/java/namenotfoundunica/houseworkcalendar/SchermataIniziale.java
+++ app/src/main/java/namenotfoundunica/houseworkcalendar/SchermataIniziale.java
@@ -3,6 +3,8 @@ package namenotfoundunica.houseworkcalendar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +13,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class SchermataIniziale extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,8 @@ public class SchermataIniziale extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
     }
 
     @Override
@@ -44,13 +50,13 @@ public class SchermataIniziale extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
