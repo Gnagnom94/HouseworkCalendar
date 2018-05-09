377d99e9db0a89a4ce62a8df79ef501b33ec92fb
Pitta37 <marcopittau@live.it>
2018-05-09 18:26:09 +0200

    Aggiunto fragments dell'imput della data tramite calendario e input dell'ora tramite orologio

--- app/src/main/java/namenotfoundunica/houseworkcalendar/AggiuntaEvento.java
+++ app/src/main/java/namenotfoundunica/houseworkcalendar/AggiuntaEvento.java
@@ -2,23 +2,40 @@ package namenotfoundunica.houseworkcalendar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
-public class AggiuntaEvento extends AppCompatActivity {
 
+import java.util.Calendar;
+
+import Fragments.DatePickerFragment;
+import Fragments.TimePickerFragment;
+
+public class AggiuntaEvento extends AppCompatActivity
+{
+    public int inputAnno;
+    public int inputMese;
+    public int inputGiorno;
+    public int inputOra;
+    public int inputMin;
+
+    public Button dataButton;
+    public Button timeButton;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aggiunta_evento);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setupActionBar();
+        dataButton = findViewById(R.id.TextDate);
+        timeButton = findViewById(R.id.TextTime);
     }
 
     private void setupActionBar()
@@ -42,5 +59,16 @@ public class AggiuntaEvento extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View v)
+    {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
 
 }
--- app/src/main/java/namenotfoundunica/houseworkcalendar/SchermataIniziale.java
+++ app/src/main/java/namenotfoundunica/houseworkcalendar/SchermataIniziale.java
@@ -147,7 +147,8 @@ public class SchermataIniziale extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_schermata_iniziale, menu);
 
