a9f7c22520afa758c1bfb390d2da2ac213555932
Deocad <alessandro.caddeo.studio@gmail.com>
2018-06-06 17:30:19 +0200

    Aggiunta funzionalità per la ListView su AggiuntaSondaggio(Bottone su Crea Sondaggio non funziona)

--- app/src/main/java/namenotfoundunica/houseworkcalendar/activity/AggiuntaSondaggio.java
+++ app/src/main/java/namenotfoundunica/houseworkcalendar/activity/AggiuntaSondaggio.java
@@ -1,21 +1,23 @@
 package namenotfoundunica.houseworkcalendar.activity;
 
-import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,15 +38,26 @@ public class AggiuntaSondaggio extends AppCompatActivity
 
         final TextView titolo_sondaggio = (TextView) findViewById(R.id.titolo_sondaggio);
         final TextView descrizione_sondaggio = (TextView) findViewById(R.id.descrizione_sondaggio);
+        final TextView risposta1 = (TextView) findViewById(R.id.Q1);
+        final TextView risposta2 = (TextView) findViewById(R.id.addQ2);
         Button send_survey = (Button) findViewById(R.id.send_survey);
-
-
+        ListView lista_risposte = (ListView) findViewById(R.id.ListAnswer);
         final List<String> risposte = new ArrayList<String>();
+
+        CustomAdapter custom = new CustomAdapter(AggiuntaSondaggio.this, R.layout.custom_addq_layout,risposte);
+
+
+        lista_risposte.setAdapter(custom);
+
+
+
         send_survey.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
+                risposte.add(risposta1.getText().toString());
+                risposte.add(risposta2.getText().toString());
                 Sondaggio survey = new Sondaggio(titolo_sondaggio.getText().toString(),
                                                 descrizione_sondaggio.getText().toString(),
                                                 "wait", GestioneSondaggi.lstSondaggio.size()+1, risposte);
@@ -58,6 +71,65 @@ public class AggiuntaSondaggio extends AppCompatActivity
 
 
     }
+    public class CustomAdapter extends ArrayAdapter
+    {
+        private Context mContext;
+        private List<String> risposte;
+        private LayoutInflater inflater;
+
+        public CustomAdapter(@NonNull Context mContext, int resource, List<String> risposte)
+        {
+            super(mContext, resource, risposte);
+            this.risposte = risposte;
+            inflater = LayoutInflater.from(mContext);
+            risposte.add("Prova");
+        }
+
+        public int getCount()
+        {
+            return risposte.size();
+        }
+        public long getItemId(int position)
+        {
+            return position;
+        }
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent)
+        {
+            if(inflater==null)
+            {
+                inflater= (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            }
+            if(convertView==null)
+            {
+                convertView=inflater.inflate(R.layout.custom_addq_layout,parent,false);
+            }
+
+            ImageButton piu = (ImageButton) convertView.findViewById(R.id.addQ);
+//            ImageButton meno = (ImageButton) convertView.findViewById(R.id.removeQ);
+//            meno.setVisibility(View.GONE);
+
+            piu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    Toast.makeText(AggiuntaSondaggio.this,"Prova",Toast.LENGTH_SHORT);
+                }
+            });
+
+
+
+
+
+
+
+            return convertView;
+        }
+
+
+
+    }
+
 
     private void setupActionBar()
     {
